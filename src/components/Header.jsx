5479dfe289699398e3ce752c0cd37d09e7ec9840
Pedro Nest <pedronestjp@gmail.comgit config --global user.name Pedro>
2023-06-06 05:33:15 -0300

    change profile area

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,108 +8,108 @@ import Navbar from "./Navbar";
 import ProductsSearch from "./ProductsSearch";
 
 function Header() {
-const { user } = useAuth();
+  const { user } = useAuth();
 
-return (
-<div className="navbarhome">
-<Navbar />
-<Link to="/" title="clique para ir para a tela inicial">
-<img
-       className="logoNestHeaderHome"
-       src="https://firebasestorage.googleapis.com/v0/b/nest-informatica.appspot.com/o/images%2FlogoNestNew.png?alt=media&token=ffd0bd44-299a-4604-b341-d2805997cac2"
-       alt="logo da Nest Informática"
-     />
-</Link>
-
-  <ProductsSearch />
-
-  <div className="areaProfile">
-    {user ? (
-      <Link
-        to="/userProfile"
-        alt="iconProfile"
-        title="Ir para o seu perfil"
-        className="iconProfileHome"
-      >
-        <BsPersonCircle />
+  return (
+    <div className="navbarhome">
+      <Navbar />
+      <Link to="/" title="clique para ir para a tela inicial">
+        <img
+          className="logoNestHeaderHome"
+          src="https://firebasestorage.googleapis.com/v0/b/nest-informatica.appspot.com/o/images%2FlogoNestNew.png?alt=media&token=ffd0bd44-299a-4604-b341-d2805997cac2"
+          alt="logo da Nest Informática"
+        />
       </Link>
-    ) : (
-      <Link
-        to="/login"
-        alt="iconProfile"
-        title="Ir para o seu perfil"
-        className="iconProfileHome"
-        onClick={() => {
-          alert("Faça seu Login ou cadastre-se");
-        }}
-      >
-        <BsPersonCircle />
-      </Link>
-    )
-    }
+
+      <ProductsSearch />
+
+      <div className="areaProfile">
+        {user ? (
+          <Link
+            to="/userProfile"
+            alt="iconProfile"
+            title="Ir para o seu perfil"
+            className="iconProfileHome"
+          >
+            <BsPersonCircle />
+          </Link>
+        ) : (
+          <Link
+            to="/login"
+            alt="iconProfile"
+            title="Ir para o seu perfil"
+            className="iconProfileHome"
+            onClick={() => {
+              alert("Faça seu Login ou cadastre-se");
+            }}
+          >
+            <BsPersonCircle />
+          </Link>
+        )
+        }
         {user ? (
           <span className="textProfileNavShort">
             Olá <strong> {user.displayName} </strong>
           </span>
-          ) : (
-            <span className="textProfileNavShort">
-              Faça seu{" "}
-              <Link
-                className="linkLoginNavShort"
-                id="linkLoginNavShort"
-                title="linkLoginNavShort"
-                to="/login"
-              >
-                LOGIN
-              </Link> ou{" "}
-              <Link
-                className="linkRegisterNavShort"
-                id="linkRegisterNavShort"
-                title="linkRegisterNavShort"
-                to="/signup"
-              >
-                CADASTRE-SE
-              </Link>
-            </span>
-          )
-          
-        }
-  </div>
+        ) : (
+          <span className="textProfileNavShort">
+            Faça seu{" "}
+            <Link
+              className="linkLoginNavShort"
+              id="linkLoginNavShort"
+              title="linkLoginNavShort"
+              to="/login"
+            >
+              LOGIN
+            </Link> ou{" "}
+            <Link
+              className="linkRegisterNavShort"
+              id="linkRegisterNavShort"
+              title="linkRegisterNavShort"
+              to="/signup"
+            >
+              CADASTRE-SE
+            </Link>
+          </span>
+        )
 
-  <div className="iconsNavHome">
-    <span>
-      <Link
-        to="/cart"
-        className="icon-shoppingCart"
-        id="iconShoppingCart"
-        title="Ir para o seu carrinho"
-      >
-        <FaShoppingCart />
-      </Link>
-    </span>
-    <span>
-      <a
-        href="https://api.whatsapp.com/message/JVU7KU5D3563D1?autoload=1&app_absent=0"
-        className="icon-support"
-        id="iconSupporte"
-        title="Fale conosco"
-      >
-        <RiCustomerService2Fill />
-      </a>
-    </span>
-    <span>
-      <Link
-        to="/favorites"
-        className="favoriteIconNav"
-        id="favoriteIconNav"
-        title="Seus produtos favoritos"
-      >
-        <BsHeartFill />
-      </Link>
-    </span>
-  </div>
-</div>
-);
+        }
+      </div>
+
+      <div className="iconsNavHome">
+        <span>
+          <Link
+            to="/cart"
+            className="icon-shoppingCart"
+            id="iconShoppingCart"
+            title="Ir para o seu carrinho"
+          >
+            <FaShoppingCart />
+          </Link>
+        </span>
+        <span>
+          <a
+            href="https://api.whatsapp.com/message/JVU7KU5D3563D1?autoload=1&app_absent=0"
+            className="icon-support"
+            id="iconSupporte"
+            title="Fale conosco"
+          >
+            <RiCustomerService2Fill />
+          </a>
+        </span>
+        <span>
+          <Link
+            to="/favorites"
+            className="favoriteIconNav"
+            id="favoriteIconNav"
+            title="Seus produtos favoritos"
+          >
+            <BsHeartFill />
+          </Link>
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
