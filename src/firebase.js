68a2376488c865d4177ee9e467292d2a78e27991
Pedro Nest <pedronestjp@gmail.com>
2023-07-27 04:06:31 -0300

    add analytics

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore} from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -16,5 +17,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
+const analytics = getAnalytics(app);
 
-export { auth, db, storage };
+export { auth, db, storage, analytics };
