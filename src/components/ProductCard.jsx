e9dbd9eedb220040a10360d8cda135b9efc0890e
Pedro Nest <pedronestjp@gmail.com>
2023-10-02 15:49:05 -0300

    fix error when click btn favorite in the home pagfix error when click btn favorite in the home page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,62 +1,99 @@
-import React, { useEffect, useState } from 'react';
-import { BsHeartFill, BsHeart } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
-import '../styles/Home.css';
-import { db } from '../firebase';
-import { collection, doc, getDoc, setDoc } from 'firebase/firestore';
-import { useAuth } from '../pages/contexts/AuthProvider';
+import React, { useEffect, useState } from "react";
+import { BsHeartFill, BsHeart } from "react-icons/bs";
+import { Link, useNavigate } from "react-router-dom";
+import "../styles/Home.css";
+import { db } from "../firebase";
+import { collection, doc, getDoc, setDoc } from "firebase/firestore";
+import { useAuth } from "../pages/contexts/AuthProvider";
 
-const ProductCard = ({ id, name, price, oldPrice, installmentPrice, imageUrl, description }) => {
+const ProductCard = ({
+  id,
+  name,
+  price,
+  oldPrice,
+  installmentPrice,
+  imageUrl,
+  description,
+}) => {
   const [favorites, setFavorites] = useState([]);
   const [isFavorite, setIsFavorite] = useState(false);
-  const { user } = useAuth()
+  const { user } = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!user) return;
-    const favoritesDoc = doc(collection(db, 'favorites'), user.uid);
-    getDoc(favoritesDoc).then((doc) => {
-      if (doc.exists()) {
-        const { products } = doc.data();
-        setFavorites(products);
-        setIsFavorite(products.includes(id))
-      }
-    })
+    const favoritesDoc = doc(collection(db, "favorites"), user.uid);
+    getDoc(favoritesDoc)
+      .then((doc) => {
+        if (doc.exists()) {
+          const { products } = doc.data();
+          setFavorites(products);
+          setIsFavorite(products.includes(id));
+        }
+      })
       .catch((error) => {
-        console.log('Error getting document:', error);
+        console.log("Error getting document:", error);
       });
   }, [user, id]);
 
   const handleAddToFavorites = (productId) => {
-    const updatedFavorites = isFavorite ? favorites.filter((id) => id !== productId) : [...favorites, productId];
-    const favoritesDoc = doc(collection(db, 'favorites'), user.uid);
+    const updatedFavorites = isFavorite
+      ? favorites.filter((id) => id !== productId)
+      : [...favorites, productId];
+    const favoritesDoc = doc(collection(db, "favorites"), user.uid);
     setDoc(favoritesDoc, { products: updatedFavorites })
       .then(() => {
         setFavorites(updatedFavorites);
         setIsFavorite(!isFavorite);
       })
       .catch((error) => {
-        console.error('Error adding product to favorites:', error);
+        console.error("Error adding product to favorites:", error);
       });
   };
 
   return (
     <div className="hl-1 styleBox">
       <button
-        onClick={() => handleAddToFavorites(id)}
-        className='favoriteIcon'
+        onClick={() =>
+          user !== null
+            ? handleAddToFavorites(id)
+            : (alert("faça seu login para poder favoritar itens"),
+              navigate("/login"))
+        }
+        className="favoriteIcon"
         alt="Icone Favoitos"
-        title={isFavorite ? 'Remover dos favoritos' : 'Adicionar aos favoritos'}>
-        {isFavorite ? <BsHeartFill style={{ color: '#e20100', height: '1.5rem', width: '1.5rem', backgroundColor: 'white' }} /> : <BsHeart style={{ height: '1.5rem', width: '1.5rem', backgroundColor: 'white' }} />}
+        title={isFavorite ? "Remover dos favoritos" : "Adicionar aos favoritos"}
+      >
+        {isFavorite ? (
+          <BsHeartFill
+            style={{
+              color: "#e20100",
+              height: "1.5rem",
+              width: "1.5rem",
+              backgroundColor: "white",
+            }}
+          />
+        ) : (
+          <BsHeart
+            style={{
+              height: "1.5rem",
+              width: "1.5rem",
+              backgroundColor: "white",
+            }}
+          />
+        )}
       </button>
-      <Link to='/'>
+      <Link to="/">
         <img className="img-hl-1" src={imageUrl} alt={name} />
       </Link>
       <span>{name}</span>
       <span className="oldPrice"> DE: {oldPrice},00 POR:</span>
       <span className="currentPrice">R${price},00</span>
       <span className="installmentPrice">10x DE R${installmentPrice},00</span>
-      <span className='descriptionProduct'>{description}</span>
-      <Link className='button-buy' to={`/products/${id}`}>Ver Detalhes</Link>
+      <span className="descriptionProduct">{description}</span>
+      <Link className="button-buy" to={`/products/${id}`}>
+        Ver Detalhes
+      </Link>
     </div>
   );
 };
