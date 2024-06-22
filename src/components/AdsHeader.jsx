0ee9e2ce5fbd88eeb4eef0cf9f49176c2b1b6069
Pedro Silva <pedronestjp@gmail.com>
2024-06-22 09:55:20 -0300

    refac: AdsHeader

--- src/components/AdsHeader.jsx
+++ src/components/AdsHeader.jsx
@@ -1,25 +1,24 @@
 import { Link } from "react-router-dom";
 import * as RiIcons from 'react-icons/ri'
 import '../styles/AdsHeader.css'
-import {CarroselAds} from "./Carrosel";
-
+import { CarroselAds } from "./Carrosel";
 
 const AdsHeader = () => {
-    return(
-  <div>
-    <section className="adsDiv">
-<CarroselAds/>
-    </section>
-    <span className="chatWhatsapp">
-      <Link
-        to="https://api.whatsapp.com/message/JVU7KU5D3563D1?autoload=1&app_absent=0"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <RiIcons.RiWhatsappFill alt="Chat do Whatsapp" className="icon-chat" />
-      </Link>
-    </span>
-  </div>
+  return (
+    <>
+      <section className="adsDiv">
+        <CarroselAds />
+      </section>
+      <span className="chatWhatsapp">
+        <Link
+          to={process.env.REACT_APP_URL_WHATSAPP}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <RiIcons.RiWhatsappFill alt="Chat do Whatsapp" className="icon-chat" />
+        </Link>
+      </span>
+    </>
   )
 }
 export default AdsHeader
