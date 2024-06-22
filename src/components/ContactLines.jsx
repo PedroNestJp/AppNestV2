4758c3b08259dec6a4edf8f0315cd9a2e91fa955
Pedro Silva <pedronestjp@gmail.com>
2024-06-22 16:20:33 -0300

    refac: contact lines

--- src/components/ContactLines.jsx
+++ src/components/ContactLines.jsx
@@ -1,88 +1,32 @@
-import { Link } from "react-router-dom"
-import { iconFacebook, iconGmail, iconInstagram, iconLinkedin, iconTelegram, iconWhatsapp, iconYoutube  } from "../img/imgs"
-import "../styles/ContactLines.css"
+import { Link } from "react-router-dom";
+import { iconFacebook, iconGmail, iconInstagram, iconLinkedin, iconTelegram, iconWhatsapp, iconYoutube } from "../img/imgs";
+import "../styles/ContactLines.css";
 
-const ContactLine1 = () => {
-    return(
-    <div className="contactsLine" id="contactsLineHome">
-    <span> 
-        <Link href="https://www.facebook.com/nestinformatica/"> 
-            <img id="iconFacebook" className="social-media" srcSet={iconFacebook} alt=""/>
-        </Link>
-    </span>
-    <span>
-      <Link href="#"> 
-        <img className="social-media" srcSet={iconGmail} alt=""/> 
-      </Link>
-    </span>
-    <span>
-      <Link href="https://www.instagram.com/nestinformatica/"> 
-        <img className="social-media" srcSet={iconInstagram}  alt=""/> 
-      </Link>
-    </span>
-    <span> 
-      <Link  href="https://www.linkedin.com/company/nestinformatica/"> 
-        <img className="social-media" srcSet={iconLinkedin} alt=""/> 
-      </Link>
-    </span>
-    <span> 
-      <Link href="#"> 
-       <img className="social-media" srcSet={iconTelegram} alt=""/> 
-      </Link>
-    </span>
-    <span> 
-      <Link href="#"> 
-        <img className="social-media" srcSet={iconWhatsapp} alt=""/> 
-      </Link>
-    </span>
-    <span> 
-      <Link href="https://www.youtube.com/channel/UCBH2a3RXY619wpctYJU8IbQ"> 
-          <img className="social-media" srcSet={iconYoutube} alt=""/>  
-      </Link>
-    </span>
-</div>
-)
-}
+const socialMediaLinks = [
+  { href: "https://www.facebook.com/nestinformatica/", srcSet: iconFacebook, alt: "Facebook", id: "iconFacebook" },
+  { href: "#", srcSet: iconGmail, alt: "Gmail" },
+  { href: "https://www.instagram.com/nestinformatica/", srcSet: iconInstagram, alt: "Instagram" },
+  { href: "https://www.linkedin.com/company/nestinformatica/", srcSet: iconLinkedin, alt: "Linkedin" },
+  { href: "#", srcSet: iconTelegram, alt: "Telegram" },
+  { href: "#", srcSet: iconWhatsapp, alt: "Whatsapp" },
+  { href: "https://www.youtube.com/channel/UCBH2a3RXY619wpctYJU8IbQ", srcSet: iconYoutube, alt: "Youtube" }
+];
 
-const ContactLine2 = () =>{
-    return(
-        <div className="contactsLine-2">
-        <span>
-        <Link href="https://www.facebook.com/nestinformatica/"> 
-                <img id="iconFacebook" className="social-media" srcSet={iconFacebook} alt=""/>
-            </Link>
-        </span> 
-        <span> 
-          <Link href="#"> 
-            <img className="social-media" srcSet={iconGmail} alt=""/> 
+const ContactLine = ({ className, id }) => {
+  return (
+    <div className={className} id={id}>
+      {socialMediaLinks.map((link, index) => (
+        <span key={index}>
+          <Link to={link.href}>
+            <img id={link.id} className="social-media" srcSet={link.srcSet} alt={link.alt} />
           </Link>
         </span>
-        <span> 
-          <Link href="https://www.instagram.com/nestinformatica/"> 
-            <img className="social-media" srcSet={iconInstagram}  alt=""/> 
-          </Link>
-        </span>
-        <span> 
-          <Link  href="https://www.linkedin.com/company/nestinformatica/"> 
-            <img className="social-media" srcSet={iconLinkedin} alt=""/> 
-          </Link>
-        </span>
-        <span> 
-          <Link href="#"> 
-           <img className="social-media" srcSet={iconTelegram} alt=""/> 
-          </Link>
-        </span>
-        <span> 
-          <Link href="#"> 
-            <img className="social-media" srcSet={iconWhatsapp} alt=""/> 
-          </Link>
-        </span>
-        <span> 
-          <Link href="https://www.youtube.com/channel/UCBH2a3RXY619wpctYJU8IbQ"> 
-              <img className="social-media" srcSet={iconYoutube} alt=""/>  
-          </Link>
-        </span>
-  </div>
-)
-}
-export {ContactLine1, ContactLine2 }
+      ))}
+    </div>
+  );
+};
+
+const ContactLine1 = () => <ContactLine className="contactsLine" id="contactsLineHome" />;
+const ContactLine2 = () => <ContactLine className="contactsLine-2" />;
+
+export { ContactLine1, ContactLine2 };
