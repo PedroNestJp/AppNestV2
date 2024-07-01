1174b09cdc99314ead65ad71d30342d22f0a8845
Pedro Silva <pedronestjp@gmail.com>
2024-07-01 02:35:17 -0300

    fix: footer title çonks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,14 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import '../styles/Footer.css';
-
-const footerData = [
-  {
-    type: "institutional",
-    title: "Institutional",
-    links: [
-      { to: "/aboutUs", title: "Quem somos" },
-      { to: "/terms", title: "Termos e Condições de Venda" },
-      { to: "/exchangePolicy", title: "Política de Troca e Devoluções" },
-    ],
-  },
-  {
-    type: "doubts",
-    title: "Dúvidas",
-    links: [
-      { to: "/howToBuy", title: "Como comprar" },
-      { to: "/delivery", title: "Prazos e entregas" },
-      { to: "/paymentMethods", title: "Formas de Pagamentos" },
-    ],
-  },
-  {
-    type: "help",
-    title: "Ajuda",
-    links: [
-      { to: "/tutorialVideos", title: "Vídeos Tutoriais" },
-      { to: "/productHandling", title: "Manuseio do Produto" },
-      { to: "https://api.whatsapp.com/message/JVU7KU5D3563D1?autoload=1&app_absent=0", title: "Fale Conosco", external: true },
-    ],
-  },
-];
+import { footerInfo } from '../data/footerInfo';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="info-footer">
         <div className="texts-info-footer">
-          {footerData.map((section) => (
+          {footerInfo.map((section) => (
             <FooterSection key={section.type} section={section} />
           ))}
         </div>
@@ -48,7 +19,7 @@ const Footer = () => {
 
 const FooterSection = ({ section }) => (
   <div className={section.type}>
-    <p>{section.title}</p>
+    <h3 className={"title-footer"}>{section.title}</h3>
     <ul>
       {section.links.map((link, index) => (
         <FooterLink key={index} link={link} />
---
+++ src/data/footerInfo.js
@@ -0,0 +1,29 @@
+export const footerInfo = [
+    {
+      type: "institutional",
+      title: "Institutional",
+      links: [
+        { to: "/aboutUs", title: "Quem somos" },
+        { to: "/terms", title: "Termos e Condições de Venda" },
+        { to: "/exchangePolicy", title: "Política de Troca e Devoluções" },
+      ],
+    },
+    {
+      type: "doubts",
+      title: "Dúvidas",
+      links: [
+        { to: "/howToBuy", title: "Como comprar" },
+        { to: "/delivery", title: "Prazos e entregas" },
+        { to: "/paymentMethods", title: "Formas de Pagamentos" },
+      ],
+    },
+    {
+      type: "help",
+      title: "Ajuda",
+      links: [
+        { to: "/tutorialVideos", title: "Vídeos Tutoriais" },
+        { to: "/productHandling", title: "Manuseio do Produto" },
+        { to: "https://api.whatsapp.com/message/JVU7KU5D3563D1?autoload=1&app_absent=0", title: "Fale Conosco", external: true },
+      ],
+    },
+  ];
